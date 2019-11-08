635f72feb82f3da4f905102a62ba09abd8076dfd
Navodit Maheshwari <navoditmaheshwari@gmail.com>
2019-11-08 08:28:14 -0800

    Pythagoras

--- Viewcontrollers/ViewController.h
+++ Viewcontrollers/ViewController.h
@@ -9,14 +9,6 @@
 #import <UIKit/UIKit.h>
 
 // Classic C style defines for Operator control
-#define PLUS 0
-#define MINUS 1
-#define MULTIPLY 2
-#define DIVIDE 3
-#define MODULO 4
-#define SQRT 5
-#define EXPONENT 6
-#define SQUARE 7
 
 @interface ViewController : UIViewController
 /* Section 1: Classic C style variables to support calculation */
